9dedbfe18ab1d7d40e3fe98bc177b6d8f2881d55
pranavjha04 <pranavjha2005@outlook.com>
2025-12-18 19:28:23 +0530

    Added more pages

--- WEB-INF/src/controllers/ManageProfileServlet.java
+++ WEB-INF/src/controllers/ManageProfileServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import java.io.IOException;
 
 @WebServlet("/manage_profile.do")
-public class OperatorDashboardServlet extends HttpServlet {
+public class ManageProfileServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
         HttpSession session = request.getSession();
